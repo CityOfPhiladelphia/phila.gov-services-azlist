08e6d3eaa78b2c8fd5ccdb5941de498ab8c3c5df
Daniel Faria <daniel.faria@phila.gov>
2019-01-09 15:27:11 -0500

    Added option to pass defaults.

--- src/AzList.vue
+++ src/AzList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
-    <div class="search-filter" v-if="searchBox">
-      <input type="text" v-model="searchValue" :placeholder="searchPlaceholder" @keyup="updateResultsList()">
+  <div id="azlist-component">
+    <div class="search-filter" v-if="options.searchBox">
+      <input type="text" v-model="options.searchValue" :placeholder="options.labels.searchPlaceholder" @keyup="updateResultsList()">
     </div>
-    <div v-if="azAnchors && azGroup" class="az-anchors">
+    <div v-if="options.azAnchors && options.azGroup" class="az-anchors">
       <ul>
         <li v-for="letter in alphabetLetters" :key="letter">
           <a href="#" v-scroll-to="'#l-' + letter" :disabled="isLetterInResults(letter)">{{ letter }}</a>
@@ -11,28 +11,29 @@
       </ul>
     </div>
     <div class="checkboxes-filter">
+      {{ options.labels.filterByText }}
       <ul>
         <li>
           <input id="all" type="checkbox" @change="uncheckAllCheckboxes();updateResultsList()" :checked="defaultCheckboxChecked">
-          <label for="all">{{ defaultCheckboxLabel }}</label>
+          <label for="all">{{ options.labels.defaultCheckboxLabel }}</label>
         </li>
-        <li v-for="listItem in checkboxList" :key="listItem" >
-          <input type="checkbox" :value="listItem" :id="listItem" v-model="checkedItems" @change="updateResultsList()">
-          <label :for="listItem">{{ listItem }}</label>
+        <li v-for="listItem in categories" :key="listItem.slug" >
+          <input type="checkbox" :value="listItem.slug" :id="listItem.slug" v-model="checkedItems" @change="updateResultsList()">
+          <label :for="listItem.slug">{{ listItem.name }}</label>
         </li>
       </ul>
     </div>
     <div class="results">
       <h2>Results</h2>
       <template v-if="hasResults()">
-        <template v-if="azGroup">
+        <template v-if="options.azGroup">
           <div v-for="(list, letter) in resultsList" :key="letter">
             <div class="letter-block" :id="'l-' + letter">
               <div class="letter">
                 {{ letter }}
               </div>
               <div v-for="(listItem, index) in list" :key="'g-' + letter + index">
-                <a :href="listItem.link">{{ listItem.label }}</a>
+                <a :href="listItem.link">{{ listItem.title }}</a>
                 <p>{{ listItem.desc }}</p>
               </div>
             </div>
@@ -40,13 +41,13 @@
         </template>
         <template v-else>
           <div v-for="(listItem, lIndex) in resultsList" :key="'l-' + lIndex">
-            <a :href="listItem.link">{{ listItem.label }}</a>
+            <a :href="listItem.link">{{ listItem.title }}</a>
             <p>{{ listItem.desc }}</p>
           </div>
         </template>  
       </template>
       <template v-else>
-        {{ noResultsMsg }}
+        {{ options.labels.noResultsMsg }}
       </template>
     </div>
   </div>
@@ -56,6 +57,8 @@
 import VueFuse from 'vue-fuse'
 import Vue from 'vue'
 import VueScrollTo from 'vue-scrollto'
+import deepMerge from 'lodash/merge'
+
 
 Vue.use(VueScrollTo, {
   container: "body",
@@ -76,55 +79,96 @@ Vue.use(VueFuse)
 export default {
   name: 'azlist',
   props: {
-    list: {
-      type: Array,
+    categories: {
+      type: [Array],
       default: () => {
         return [{
-          label: 'Sample Entry Label',
+          name: 'Sample Category 1',
+          slug: 'cat-1',
+        }]
+      },
+      validator: (value) => {
+
+        let sample = value[0]
+
+        if (
+          sample.hasOwnProperty('name') &&
+          sample.hasOwnProperty('slug')
+        ) {
+          return true
+        }
+        
+        console.log(`The data must be an array of objects with the following keys: name, slug`)
+
+        return false
+
+      },
+    },
+    list: {
+      type: [Array],
+      default: () => {
+        return [{
+          title: 'Sample Entry Label',
           desc: 'Sample Entry Desc... you have no data',
           link: 'http://www.google.com',
-          tags:['Tag1', 'Tag2'],
+          categories: ['cat-1'],
         }]
-      }
+      },
+      validator: (value) => {
+
+        let sample = value[0]
+
+        if (
+          sample.hasOwnProperty('title') &&
+          sample.hasOwnProperty('desc') &&
+          sample.hasOwnProperty('link') &&
+          sample.hasOwnProperty('categories')
+        ) {
+          return true
+        }
+        
+        console.log(`The data must be an array of objects with the following keys: title, desc, link, categories`)
+
+        return false
+
+      },
+    },
+    propOptions: {
+      type: [Object],
     }
   },
   data() {
     return {
-      searchBox: true, //display search box
-      searchValue: '',
-      searchPlaceholder: 'Search services',
-      fuseSearchOptions: {
-        keys: [
-          'label',
-          'desc',
-        ],
-        defaultAll: false,
-        tokenize: true, 
-        matchAllTokens: true,
-        threshold: 0.1,
+      options: {
+        azAnchors: true, //display a-z anchors, azGroup must also be true
+        azGroup: true, //group results by a-z
+        fuseSearchOptions: {
+          defaultAll: false,
+          keys: [
+            'title',
+            'desc',
+          ],
+          matchAllTokens: true,
+          threshold: 0.1,
+          tokenize: true, 
+        },
+        labels: {
+          noResultsMsg: `Sorry, we couldn't find anything for that search. Please try different terms.`,
+          searchPlaceholder: 'Search services',
+          defaultCheckboxLabel: 'All Services',
+          filterByText: 'Filter by service category',
+        },
+        searchBox: true, //display search box
+        searchValue: '',
       },
-      azAnchors: true, //display a-z anchors, azGroup must also be true
-      azGroup: true, //group results by a-z
       alphabet: 'abcdefghijklmnopqrstuvwxyz',
-      resultsList: [],
-      noResultsMsg: `Sorry, we couldn't find anything for that search. Please try different terms.`,
-      defaultCheckboxChecked: true,
-      defaultCheckboxLabel: 'All Services',
       checkedItems: [],
-      checkboxList: [
-        'Crime, law & justice',
-        'Birth, marriage & life events',
-        'Business & self-employment',
-        'Cars, parking & transportation',
-        'Culture & recreation',
-        'Diversity, inclusion & immigration',
-        'Education & learning',
-        'Mental & physical health',
-        'Payments, assistance & taxes'
-      ],
+      defaultCheckboxChecked: true,
+      resultsList: [],
     }
   },
-  mounted() {
+  created() {
+    deepMerge(this.options, this.propOptions)
     this.init()
   },
   computed: {
@@ -134,11 +178,17 @@ export default {
     }
   },
   methods: {
+    gettype(item) {
+      console.log(typeof item)
+    },
     init() {
+      
       this.resultsList = this.list
       this.updateResultsList()
+
     },
     hasResults() {
+      
       //test if results are not grouped
       if (Array.isArray(this.resultsList) && this.resultsList.length > 0) {
         return true
@@ -159,11 +209,11 @@ export default {
       filteredList = await this.filterCheckbox(filteredList)
 
       //search filter
-      if (this.searchBox) {
+      if (this.options.searchBox) {
         filteredList = await this.filterSearch(filteredList)
       }
       
-      if (this.azGroup) {
+      if (this.options.azGroup) {
         filteredList = await this.groupAzList(filteredList)
       } else {
         this.sortResultsListByLabel(filteredList)
@@ -174,10 +224,10 @@ export default {
     },
     sortResultsListByLabel(list) {
       return list.sort((a, b) => {
-        if (a.label < b.label) {
+        if (a.title < b.title) {
           return -1
         }
-        if (a.label > b.label) {
+        if (a.title > b.title) {
           return 1
         }
         return 0
@@ -189,8 +239,8 @@ export default {
       let orderedCombinedList = {}
       
       list.forEach((item, index) => {
-        let letter = item.label.charAt(0)
-
+        let letter = item.title.charAt(0)
+        
         if (!combinedList.hasOwnProperty(letter)) {
           combinedList[letter] = []
           combinedList[letter].push(item)
@@ -213,7 +263,7 @@ export default {
       if (this.checkedItems.length > 0) {
         this.uncheckDefaultCheckbox()
         return list.filter((listItem) => {
-          return listItem.tags.some((tag) => {
+          return listItem.categories.some((tag) => {
             return this.checkedItems.includes(tag)
           }, this)
         })
@@ -224,8 +274,8 @@ export default {
       
     },
     filterSearch(list) {
-      if (this.searchValue != '') {
-        return this.$search(this.searchValue, list, this.fuseSearchOptions).then(results => {
+      if (this.options.searchValue != '') {
+        return this.$search(this.options.searchValue, list, this.options.fuseSearchOptions).then(results => {
           return results
         })
       } else {
@@ -240,7 +290,7 @@ export default {
       this.defaultCheckboxChecked = false;
     },
     isLetterInResults(letter) {
-      if (this.azAnchors && this.azGroup) {
+      if (this.options.azAnchors && this.options.azGroup) {
         return !this.resultsList.hasOwnProperty(letter)
       }
     },
--- src/main.js
+++ src/main.js
@@ -1,8 +1,58 @@
 import Vue from 'vue'
-import App from './App.vue'
+import AzList from './AzList.vue'
+import axios from 'axios'
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
+async function getAzListCategories() {
+  return axios.get('https://staging-www.phila.gov/wp-json/services/v1/categories').then((response) => {
+    return response.data
+  })
+}
+
+async function getAzList() {
+  return axios.get('https://staging-www.phila.gov/wp-json/services/v1/directory').then((response) => {
+    return response.data.map((item) => {
+
+      let categories = item.categories.map((cat) => {
+        
+        if (cat) {
+          return cat.slug
+        }
+
+        return ''
+        
+      })
+
+      return {
+        title: item.title,
+        link: item.link,
+        desc: !item.desc && '',
+        categories
+      }
+    })
+  })
+}
+
+async function initAzList() {
+
+  const categories = await getAzListCategories()
+  const list = await getAzList()
+
+  new Vue({
+    render(h) {
+      return h(AzList, {
+        props: {
+          categories,
+          list
+        },
+      })
+    },
+  }).$mount('#app')
+
+}
+
+initAzList()
+
